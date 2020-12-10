d7b8ffa27ebf711c2035b64d3431afca7bdfade9
binsust98 <binsust98@gmail.com>
2020-12-10 14:04:28 +0530

    learn dit art app

---
+++ app/src/main/java/com/binsu/learndiyarts/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.binsu.learndiyarts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
